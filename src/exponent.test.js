80da6b807b72317aada1312ca9b683fb2e098f68
Coy <patrick.bryan.peralta@gmail.com>
2020-07-10 12:51:27 -0400

    pass this test

--- src/exponent.test.js
+++ src/exponent.test.js
@@ -4,7 +4,7 @@ describe('simple operation', () => {
 
   test('follows order of operands', () => {
     expect(exponentiate(2, 1)).toEqual(2);
-  
+    expect(exponentiate(2, 2)).toEqual(4);
   });  
   
 });
