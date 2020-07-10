1d834d029a139de5dcc6791cbd9ddd8f10fe8ec8
Coy <patrick.bryan.peralta@gmail.com>
2020-07-10 12:40:43 -0400

    totally works changes - exponent

--- src/exponent.ts
+++ src/exponent.ts
@@ -1,4 +1,25 @@
+import { multiply } from "./multiply";
+import { divide } from "./divide";
 
 export function exponentiate(base : number, exponent: number) {
-    /// insert exponent function here
+
+    if (exponent === 0)
+        return 1;
+
+    else if (exponent === 1) 
+        return base;
+
+    else if (exponent < 0)
+        return multiply(
+            (new Array(exponent)).fill(
+                divide([1, base])
+            )
+        );    
+
+    else
+        return multiply(
+            (new Array(exponent)).fill(base)
+        );    
+
+    
 }
